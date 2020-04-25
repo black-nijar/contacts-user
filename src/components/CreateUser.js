01e64b6aaff3536e0063eaddb7c8db55534877a4
Nijarudeen <nijarr2020@gmail.com>
2020-04-25 13:43:51 +0530

    initial commit

---
+++ src/components/CreateUser.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CreateUser = () => {
+  return (
+    <div>
+      Click here to create an user
+      <form>
+        <div>
+          <label>Create a User</label>
+          <input/>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default CreateUser
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function User() {
+  return (
+    <div>
+      Users List
+    </div>
+  )
+}
+
+export default User
